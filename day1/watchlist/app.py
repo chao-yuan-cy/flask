259020599f9a2928f57b34faf104b663a774fcae
cy <1337103725@qq.com>
2020-04-01 17:56:52 +0800

    '第二次项目于提交'

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,13 +1,22 @@
-from flask import Flask
+from flask import Flask,url_for,render_template
 app = Flask(__name__)
 # print('nihao')
 @app.route('/')
 
 @app.route('/home')
 def index():
-    return "<h1>Hello，Flask</h1>"
+    name = '你好'
+    movies = [
+        {'title':'大赢家','year':'2020'},
+        {'title':'囧妈','year':'2020'},
+        {'title':'战狼','year':'2018'},
+        {'title':'心花路放','year':'2014'},
+        {'title':'速度与激情','year':'2018'},
+        {'title':'港囧','year':'2016'},
+    ]
+    return render_template('index.html',name=name,movies=movies)
 #动态url
 @app.route('/index/<name>')
 def home(name):
-   
+    print(url_for('home',name='hi'))
     return '<h1>hello,%s</h1>'%name
