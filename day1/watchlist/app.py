6ebef1452bb59449cfecd49452046dff86a8299e
cy <1337103725@qq.com>
2020-04-02 16:10:20 +0800

    '数据库创建完成，数据导入成功'

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix +os.path.join(app.root_path,'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
+#models
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
@@ -28,7 +28,32 @@ class Movie(db.Model):
 @app.route('/')
 
 @app.route('/home')
+#views
 def index():
+ 
+    user = User.query.first()
+    movies = Movie.query.all()
+
+    return render_template('index.html',user=user,movies=movies)
+#动态url
+@app.route('/index/<name>')
+def home(name):
+    print(url_for('home',name='hi'))
+    return '<h1>hello,%s</h1>'%name
+
+#自定义命令
+#新建data.db的数据库初始化命令
+@app.cli.command()#装饰器。注册命令
+@click.option('--drop',is_flag=True,help='删除后再创建')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('初始化数据库完成')
+
+# 项data.db中写入数据的命令
+@app.cli.command()
+def forge():
     name = '你好'
     movies = [
         {'title':'大赢家','year':'2020'},
@@ -38,18 +63,10 @@ def index():
         {'title':'速度与激情','year':'2018'},
         {'title':'港囧','year':'2016'},
     ]
-    return render_template('index.html',name=name,movies=movies)
-#动态url
-@app.route('/index/<name>')
-def home(name):
-    print(url_for('home',name='hi'))
-    return '<h1>hello,%s</h1>'%name
-
-#自定义命令
-@app.cli.command()#装饰器。注册命令
-@click.option('--drop',is_flag=True,help='删除后再创建')
-def initdb(drop):
-    if drop:
-        db.drop_all()
-    db.create_all()
-    click.echo('初始化数据库完成')
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movies = Movie(title=m['title'],year=m['year'])
+        db.session.add(movies)
+    db.session.commit()
+    click.echo('插入数据完成')
